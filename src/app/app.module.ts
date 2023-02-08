5568152bfbb56a546d7b0beb7de60b1ca863cb45
Ary <ariadna.des.garcia@gmail.com>
2023-02-08 13:23:04 -0300

    version statica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SobreMiComponent } from './component/sobre-mi/sobre-mi.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { SkillsComponent } from './component/skills/skills.component';
+import { FactsComponent } from './component/facts/facts.component';
+import { NavbarMovileComponent } from './component/navbar-movile/navbar-movile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreMiComponent,
+    NavbarComponent,
+    SkillsComponent,
+    FactsComponent,
+    NavbarMovileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/navbar-movile/navbar-movile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-movile',
+  templateUrl: './navbar-movile.component.html',
+  styleUrls: ['./navbar-movile.component.css']
+})
+export class NavbarMovileComponent {
+
+}
